eb9ed2f459d3481d0a27bf4cfe6ac955cc63f037
JeeXu <993829498@qq.com>
2020-10-02 11:18:14 +0800

    创建编写导航栏、头像组件，store

---
+++ src/components/AppBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- 头部导航栏 -->
+  <div class="app-bar">
+    <!-- 左边（可自定义图标） -->
+    <span class="app-bar_l" @click="$emit('left')">
+      <i :class="['fa', `fa-${left}`]"></i>
+    </span>
+    <!-- 中间 title -->
+    <h1>{{ title }}</h1>
+    <!-- 右边（可自定义图标） -->
+    <span class="app-bar_r" @click="$emit('right')">
+      <i :class="['fa', `fa-${right}`]"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    left: {
+      type: String,
+      default: 'chevron-left'
+    },
+    right: {
+      type: String,
+      default: 'ellipsis-v'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-bar {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  font-size: 15px;
+  z-index: 1;
+}
+h1 {
+  display: flex;
+  justify-content: center;
+  flex: 1;
+}
+span {
+  padding: 0 20px;
+}
+</style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- avatar（头像） -->
+  <div class="avatar" :class="{ avatar_selected: !!selected }">
+    <div class="avatar_face">
+      <img :src="avatar_imgUrl" alt="头像" />
+    </div>
+    <!-- 姓名 -->
+    <h2 class="avatar_name">{{ avatar_name }}</h2>
+    <!-- todo 提示 -->
+    <p class="avatar_tips">
+      Look like feed good.<br />You have {{ todayTasks.length }} tasks to do
+      today
+    </p>
+    <p class="avatar_date">TODAY : {{ today | dateString }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {
+      avatar_imgUrl: 'https://source.unsplash.com/300x300',
+      avatar_name: 'Hello, Jane',
+      today: new Date()
+    }
+  },
+  computed: {
+    ...mapState(['selected']),
+    ...mapGetters(['todayTasks'])
+  },
+  filters: {
+    dateString(val) {
+      return val
+        .toDateString()
+        .toUpperCase()
+        .replace(/(\s\d{4})$/, ', $1')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
