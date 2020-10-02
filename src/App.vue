8151e94bb00fc9e9c2b24667ff064c4be3b0dbe0
JeeXu <993829498@qq.com>
2020-10-02 11:15:52 +0800

    初始化项目，并AppBar、Avatar组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <transition>
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// @import url('https://cdn.bootcdn.net/ajax/libs/font-awesome/5.9.0/css/fontawesome.css');
+@import url('https://cdn.bootcdn.net/ajax/libs/font-awesome/4.7.0/css/font-awesome.css');
+
+body {
+  background-color: rgb(54, 54, 54);
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
